04a22e4baabcf290a54492dd9790c7d0d0adc27c
migueegomez7 <miguelangelgomezx2@gmail.com>
2022-12-01 13:42:10 +0100

    Reorganizado los urls

---
+++ estaticas/urls.py
@@ -0,0 +1,18 @@
+from django.contrib import admin
+from django.urls import path, include
+from django.conf.urls.static import static
+from PGPI import settings
+from . import views as v
+
+    
+    
+    
+    
+urlpatterns = [
+      
+    path('devoluciones/', v.devolucion),
+    path('envios/', v.envio),
+    path('atencioncliente/', v.cliente),
+    path('privacidad/', v.privacidad),
+    path('terminos/', v.terminos)
+]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
